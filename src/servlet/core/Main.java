ffb4ac54e0def8c25cf368923ed15e17c71eb7e2
marmalmstudent <marmalm@student.chalmers.se>
2017-07-28 16:05:16 +0200

    managed to launch program from servlet

--- src/servlet/core/Main.java
+++ src/servlet/core/Main.java
@@ -22,12 +22,19 @@ package servlet.core;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
 
 import servlet.implementation.JSONRead;
 
@@ -52,9 +59,43 @@ public class Main extends HttpServlet
 		// Actual logic goes here.
 		PrintWriter out = response.getWriter();
 		out.println("<h1>" + message + "</h1>");
+		
+		out.println("<APPLET codebase='classes' code='applet/implementation/GUI_UserInterface.class' width=800 height=650 archive='activation-1.1.1.jar,gnumail-providers-1.1.2.jar,json-simple-1.1.1.jar,gnumail-1.1.2.jar,inetlib-1.1.2.jar'></APPLET>");
+		out.println("<a href='./WEB-INF/classes'>Link to this folder</a>");
+
+		JSONObject ret = new JSONObject();
+		Map<String, String> rmap = (Map<String, String>) ret;
+		rmap.put("command", "load_questions");
+
+		URL url;
+		HttpURLConnection connection;
+		try {
+			url = new URL("http://localhost:8080/PROM_PREM_Collector/main");
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("POST");
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setUseCaches(false);
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			//Send request
+			OutputStream os = connection.getOutputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+			out.println(ret.toString());
+			osw.write(ret.toString());
+			osw.flush();
+			osw.close();
+			if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				out.println("Ok response");
+			} else {
+				out.println("Bad response");
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
 		try{
 			StringBuilder sb = new StringBuilder();
 			BufferedReader br = request.getReader();
@@ -62,9 +103,8 @@ public class Main extends HttpServlet
 			while ((str = br.readLine()) != null) {
 				sb.append(str);
 			}
-			System.out.println(str);
+			out.println(str);
 			String response_str = JSONRead.handleRequest(sb.toString());
-			PrintWriter out = response.getWriter();
 			out.println(response_str);
 		} catch (Exception e) {
 			e.printStackTrace();

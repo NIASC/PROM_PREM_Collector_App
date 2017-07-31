12f49b5e18d3c9feb8f9db0136b02d082797d899
marmalmstudent <marmalm@student.chalmers.se>
2017-07-31 17:21:58 +0200

    removed print nat updated start page

--- src/applet/core/Questionnaire.java
+++ src/applet/core/Questionnaire.java
@@ -120,10 +120,7 @@ public class Questionnaire
 			return;
 		Form form = new Form();
 		for (int i = 0; i < questions.getSize(); ++i)
-		{
-			System.out.println(questions.getContainer(i).getStatement());
 			form.insert(questions.getContainer(i), Form.AT_END);
-		}
 		form.jumpTo(Form.AT_BEGIN);
 		
 		ui.presentForm(form, this::saveQuestionaire, false);
